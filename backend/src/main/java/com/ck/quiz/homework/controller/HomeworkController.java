c46ac61a34b214cf31365b4234b8ec564bf997b3
chengkai3 <chengkai3@asiainfo.com>
2026-03-16 22:29:11 +0800

    fix(homework): align api route prefix with frontend

--- backend/src/main/java/com/ck/quiz/homework/controller/HomeworkController.java
+++ backend/src/main/java/com/ck/quiz/homework/controller/HomeworkController.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/homework")
+@RequestMapping({"/api/homework", "/homework"})
 public class HomeworkController
         extends BaseController<HomeworkCreateDto, HomeworkUpdateDto, HomeworkQueryDto, HomeworkDto> {
 
