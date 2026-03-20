f2b2921a2ab6d30efac8898fd6d17207b63cbb9f
chengkai3 <chengkai3@asiainfo.com>
2026-03-20 10:15:52 +0800

    feat(cron): add knowledge processing job option

--- backend/src/main/java/com/ck/quiz/cron/service/JobService.java
+++ backend/src/main/java/com/ck/quiz/cron/service/JobService.java
@@ -9,6 +9,7 @@ import com.ck.quiz.cron.exec.AbstractJob;
 import com.ck.quiz.cron.exec.LocalScriptExecJob;
 import com.ck.quiz.cron.exec.NotificationJob;
 import com.ck.quiz.cron.exec.RemoteScriptExecJob;
+import com.ck.quiz.knowledgeset.job.KnowledgeProcessingJob;
 import com.ck.quiz.cron.repository.JobRepository;
 import com.ck.quiz.cron.repository.PendingJobRepository;
 import com.ck.quiz.seq.service.SeqService;
@@ -343,7 +344,7 @@ public class JobService {
     }
 
     public List<Map<String, Object>> getJobOptions() {
-        return Arrays.stream(new Class[] { NotificationJob.class, LocalScriptExecJob.class, RemoteScriptExecJob.class, com.ck.quiz.crawler.job.WebMagicCrawlerJob.class })
+        return Arrays.stream(new Class[] { NotificationJob.class, LocalScriptExecJob.class, RemoteScriptExecJob.class, com.ck.quiz.crawler.job.WebMagicCrawlerJob.class, KnowledgeProcessingJob.class })
                 .filter(clazz -> AbstractJob.class.isAssignableFrom(clazz)
                         || AbstractAsyncJob.class.isAssignableFrom(clazz)) // 必须继承 AbstractJob
                 .filter(clazz -> !Modifier.isAbstract(clazz.getModifiers())) // 排除抽象类
